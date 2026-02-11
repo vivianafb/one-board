c901079fa29157e2557278b2d2705ba0475df119
Viviana Fajardo <vfajardo@aaxis.io>
2026-02-11 16:32:58 -0300

    feat(savings): implement selectors for calculated totals

---
+++ src/features/savings/selectors.ts
@@ -0,0 +1,11 @@
+import { SavingsStore } from "./store";
+
+// Suma el currentAmount de todas las metas activas
+export const selectTotalSaved = (state: any) => {
+  return state.goals.reduce((acc: number, goal: any) => acc + goal.currentAmount, 0);
+};
+
+// Suma el histórico mensual (si quieres ver cuánto has guardado en total en el tiempo)
+export const selectHistoricalSavings = (state: any) => {
+  return state.monthlyHistory.reduce((acc: number, item: any) => acc + item.amount, 0);
+};
